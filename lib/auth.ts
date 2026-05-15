34572140ef23c692e8b96e5d034f83fbafcde097
uiubueno <buenowiiu@gmail.com>
2026-05-15 13:21:04 -0300

    Corrigindo tipagem na sessao do auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -20,8 +20,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
     async session({ session, token }) {
       if (session.user) {
-        session.user.role = token.role as string
-        session.user.id = token.id as string
+      (session.user as any).role = token.role as string
+      (session.user as any).id = token.id as string
       }
       return session
     },
