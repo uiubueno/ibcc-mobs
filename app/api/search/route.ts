710576dd240db4237b15add373999265cd7c2ccd
uiubueno <buenowiiu@gmail.com>
2026-05-15 12:34:18 -0300

    Corrigindo API de busca limpa

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -25,7 +25,6 @@ export async function GET(req: NextRequest) {
           { sector: { contains: query, mode: 'insensitive' } }
         ]
       },
-      include: { furniture: true },
       take: 5
     })
   ])
